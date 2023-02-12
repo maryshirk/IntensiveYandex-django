c36470310662677132146f65ca814a29fd820b65
maryshirk <b06050382@gmail.com>
2023-02-12 13:56:20 +0300

    change

--- lyceum/lyceum/catalog/tests.py
+++ lyceum/lyceum/catalog/tests.py
@@ -6,3 +6,7 @@ class StaticUrlTests(TestCase):
     def test_catalog_endpoint(self):
         response = Client().get("/catalog/")
         self.assertEqual(response.status_code, 200)
+
+    def test_catalog_int_endpoint(self):
+        response = Client().get("/catalog/1/")
+        self.assertEqual(response.status_code, 200)
