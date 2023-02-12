02a16bf14df8b3fad41e0ff48190e6c3829944d8
maryshirk <b06050382@gmail.com>
2023-02-12 22:31:26 +0300

    change

--- lyceum/lyceum/catalog/urls.py
+++ lyceum/lyceum/catalog/urls.py
@@ -7,6 +7,6 @@ register_converter(converters.DateConverter, "date")
 urlpatterns = [
     path("", views.item_list),
     path("<int:pk>/", views.item_detail),
-    re_path(r"re/(?P<pk>[1-9]\d*)/$", views.item_detail),
     path("<date:dt>/<int:pk>/", views.item_data_detail),
+    re_path(r"re/(?P<pk>[1-9]\d*)/$", views.item_detail),
 ]
