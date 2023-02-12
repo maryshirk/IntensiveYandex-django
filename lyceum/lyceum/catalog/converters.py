a6c44a8af1af7e4337fb528db2eb2599849289a6
maryshirk <b06050382@gmail.com>
2023-02-12 22:13:53 +0300

    change

--- lyceum/lyceum/catalog/converters.py
+++ lyceum/lyceum/catalog/converters.py
@@ -2,11 +2,11 @@ from datetime import datetime
 
 
 class DateConverter:
-   regex = r"[0-9]{4}-[0-9]{2}-[0-9]{2}"
-   format = "%Y-%m-%d"
+    regex = r"[0-9]{4}-[0-9]{2}-[0-9]{2}"
+    format = "%Y-%m-%d"
 
-   def to_python(self, value: str):
-       return datetime.strptime(value, self.format)
+    def to_python(self, value: str):
+        return datetime.strptime(value, self.format)
 
-   def to_url(self, value: datetime):
-       return value.strftime(self.format)
+    def to_url(self, value: datetime):
+        return value.strftime(self.format)
