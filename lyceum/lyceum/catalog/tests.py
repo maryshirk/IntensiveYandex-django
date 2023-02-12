2ecac6e68e5927e6e399f2eb4534fce1de9a884f
maryshirk <b06050382@gmail.com>
2023-02-12 13:49:35 +0300

    change

--- lyceum/lyceum/catalog/tests.py
+++ lyceum/lyceum/catalog/tests.py
@@ -4,5 +4,5 @@ from django.test import Client, TestCase
 # Create your tests here.
 class StaticUrlTests(TestCase):
     def test_catalog_endpoint(self):
-        response = Client().get("/")
+        response = Client().get("/catalog/")
         self.assertEqual(response.status_code, 200)
