aae092e2e1601c86e2f7c7c33ad634b9eb27755f
maryshirk <b06050382@gmail.com>
2023-02-12 13:37:12 +0300

    change

--- lyceum/lyceum/coffee/tests.py
+++ lyceum/lyceum/coffee/tests.py
@@ -4,9 +4,9 @@ from django.test import Client, TestCase
 # Create your tests here.
 class StaticUrlTests(TestCase):
     def test_coffee_endpoint(self):
-        response = Client().get("/coffee")
+        response = Client().get("/coffee/")
         self.assertEqual(response.status_code, 418)
 
     def test_coffee_content(self):
-        response = Client().get("/coffee")
+        response = Client().get("/coffee/")
         self.assertEqual(response.content, "<body>Я чайник</body>")
