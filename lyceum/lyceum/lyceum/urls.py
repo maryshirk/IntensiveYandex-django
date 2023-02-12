63996fc298b0131d9cb5ba8999ec42b2b19c37a7
maryshirk <b06050382@gmail.com>
2023-02-12 11:39:13 +0300

    change

--- lyceum/lyceum/lyceum/urls.py
+++ lyceum/lyceum/lyceum/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 
 urlpatterns = [
@@ -24,3 +24,11 @@ urlpatterns = [
     path("about/", include("about.urls")),
     path("admin/", admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__', include(debug_toolbar.urls))
+    ]
