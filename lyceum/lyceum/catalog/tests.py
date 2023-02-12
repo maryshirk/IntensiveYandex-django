5d1419af8c2d1a0fabe0bdc1a87b19741d925b2f
maryshirk <b06050382@gmail.com>
2023-02-12 14:20:48 +0300

    change

--- lyceum/lyceum/catalog/tests.py
+++ lyceum/lyceum/catalog/tests.py
@@ -10,3 +10,7 @@ class StaticUrlTests(TestCase):
     def test_catalog_int_endpoint(self):
         response = Client().get("/catalog/1/")
         self.assertEqual(response.status_code, 200)
+
+    def test_catalog_re_int_endpoint(self):
+        response = Client().get("/catalog/re/1/")
+        self.assertEqual(response.status_code, 200)
--- lyceum/lyceum/catalog/urls.py
+++ lyceum/lyceum/catalog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.item_list),
     path("<int:pk>", views.item_detail),
+    re_path(r're/(?P<pk>[1-9]\d*)/$', views.item_detail),
 ]
