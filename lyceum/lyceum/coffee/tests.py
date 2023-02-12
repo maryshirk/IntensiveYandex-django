cb729ae0f7480e2afae81ee47ed43fb2f26a4e44
maryshirk <b06050382@gmail.com>
2023-02-12 13:47:09 +0300

    change

--- lyceum/lyceum/coffee/tests.py
+++ lyceum/lyceum/coffee/tests.py
@@ -7,11 +7,10 @@ class StaticUrlTests(TestCase):
         response = Client().get("/coffee/")
         self.assertEqual(response.status_code, 418)
 
-
     def test_coffee_content(self):
         response = Client().get("/coffee/")
         self.assertEqual(
             response.content,
             "<body>\xd0\xaf \xd1\x87"
             "\xd0\xb0\xd0\xb9\xd0\xbd\xd0\xb8\xd0\xba</body>"
-        )
+            )
